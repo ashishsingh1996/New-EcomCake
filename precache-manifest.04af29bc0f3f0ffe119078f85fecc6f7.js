4b0117fb030b0e055af05b4c44eb66542d38f47a
ashishsingh1996 <ashishvsingh1996@gmail.com>
2021-07-24 21:47:14 +0530

    added new site

---
+++ precache-manifest.04af29bc0f3f0ffe119078f85fecc6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8df4af7e10a6c9307f8f0cc9137e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02fe1ab78556443728b6",
+    "url": "/static/js/2.44abe0d0.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.44abe0d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4fe019c50b37799dce",
+    "url": "/static/js/main.1e0bc30d.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
